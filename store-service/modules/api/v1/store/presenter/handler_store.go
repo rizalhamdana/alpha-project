e34f0d25cd796c97e5a2916193acebd4dfb6ef9b
rizalhamdana <ari.gusti12@gmail.com>
2021-09-18 14:52:34 +0700

    add health check for api version 1 store service

--- store-service/modules/api/v1/store/presenter/handler_store.go
+++ store-service/modules/api/v1/store/presenter/handler_store.go
@@ -1,6 +1,8 @@
 package presenter
 
 import (
+	"net/http"
+	healthModel "rizalhamdana/alpha_project/store_service/modules/api/v1/health/model"
 	"rizalhamdana/alpha_project/store_service/modules/api/v1/store/usecase"
 
 	"github.com/labstack/echo/v4"
@@ -15,11 +17,22 @@ func NewStoreHttpHandler(storeUsecase usecase.I_StoreUsecase) *StoreHttpHandler 
 }
 
 func (h *StoreHttpHandler) Mount(group *echo.Group) {
+	group.GET("/store/check", h.CheckStoreAPI)
 	group.GET("/store/:uuid", h.GetOneStore)
 	group.POST("/store", h.CreateNewStore)
 	group.PUT("/store/:uuid", h.UpdateOneStore)
 	group.DELETE("/store/:uuid", h.DeleteOneStore)
 
+}
+
+func (h *StoreHttpHandler) CheckStoreAPI(e echo.Context) (err error) {
+	health := healthModel.Health{
+		Status:      "Running",
+		Version:     "v1",
+		ServiceName: "Store API v1",
+	}
+
+	return e.JSON(http.StatusOK, health)
 }
 
 func (h *StoreHttpHandler) GetOneStore(e echo.Context) (err error) {
